f10e0641c780a1fe1ec46c4fa55ffe1c03d0c38a
Neoboxz <neoplayz22@gmail.com>
2023-07-28 17:34:55 +0800

    commit

--- main/views.py
+++ main/views.py
@@ -12,7 +12,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.mail import send_mail
 from .models import Student, Faculty
-from .utils import generate_otp
+
 
 
 from django import forms
