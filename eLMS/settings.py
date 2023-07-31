de20a0a74dd5670a72e48d9e3f94176ec2b763c4
Neoboxz <neoplayz22@gmail.com>
2023-07-31 16:24:14 +0800

    commit

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -171,3 +171,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'prcaremanagement@outlook.com'
+EMAIL_HOST_PASSWORD = 'Dummyaccount'
+DEFAULT_FROM_EMAIL = 'prcaremanagement@outlook.com'
