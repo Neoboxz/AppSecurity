bd44a01b084727e3ea7b986e0ded4d7a3627178c
westdemon <leander.phua@gmail.com>
2023-08-15 17:42:31 +0800

    commit

--- main/admin.py
+++ main/admin.py
@@ -18,7 +18,8 @@ class StudentAdmin(admin.ModelAdmin):
     form = StudentChangeForm
     list_display = ('image_tag', 'student_id', 'name', 'email', 'department')
     search_fields = ('name', 'email')
-
+    readonly_fields = ('role',)
+    fields = ('student_id', 'name', 'email', 'password', 'role', 'course', 'photo', 'department')
     def display_circular_photo(self, obj):
         return format_html('<div style="width: 50px; height: 50px; border-radius: 50%; overflow: hidden;"><img src="{}" style="width: 100%; height: 100%;" /></div>', obj.photo.url)
 
@@ -30,6 +31,8 @@ class FacultyAdmin(admin.ModelAdmin):
     form = FacultyChangeForm
     list_display = ('image_tag', 'faculty_id', 'name', 'email', 'role')
     search_fields = ('name', 'email')
+    readonly_fields = ('role',)
+    fields = ('faculty_id', 'name', 'email', 'password', 'department', 'role', 'photo')
 
     def display_circular_photo(self, obj):
         return format_html('<div style="width: 50px; height: 50px; border-radius: 50%; overflow: hidden; display: flex; justify-content: center; align-items: center;"><img src="{}" style="width: 100%; height: 100%; object-fit: cover;" /></div>', obj.photo.url)
